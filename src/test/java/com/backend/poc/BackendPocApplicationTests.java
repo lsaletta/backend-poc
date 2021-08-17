fcf4975246c8145db2f4b41af58038a70ecdacbb
Luca Saletta <luca.salettasillero@colaborador.viajeseci.es>
2021-08-17 10:41:24 +0200

    Added project archetype

---
+++ src/test/java/com/backend/poc/BackendPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
